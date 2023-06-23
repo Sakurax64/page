975ce8751ae2e8e801b75625b3598ffcf2a4878a
sakura_x64 <lia.mueller1337@gmail.com>
2023-06-23 20:21:33 +0200

    add estrogen timer

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -48,6 +48,31 @@ window.addEventListener('DOMContentLoaded', event => {
 
     const year = new Date().getFullYear();
     const element = document.getElementById("copyright");
+    const eStart = new Date("2023-04-27T14:00:00Z")
 
     element.innerHTML = `Copyright &copy; Sakura_x64 ${year}`;
+
+    function duration() {
+        const totalSeconds = Math.trunc((Date.now() - eStart) / 1000);
+        const totalMinutes = Math.floor(totalSeconds / 60);
+        const totalHours = Math.floor(totalSeconds / 3600);
+
+        const seconds = totalSeconds % 60;
+        const minutes = totalMinutes % 60;
+        const hours = totalHours % 24;
+        const days = Math.floor(totalHours / 24);
+
+        return { d: days, h: hours, m: minutes, s: seconds };
+    }
+
+    const timer = document.getElementById("timer");
+
+    setInterval(() => {
+        timer.innerText = `${duration().d}d ${duration().h}h ${duration().m}m ${duration().s}s`;
+
+        
+
+    }, 1000);
+
+
 });
